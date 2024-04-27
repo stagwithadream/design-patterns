4976dcedaddfbcb5e180491bd6067530d517f9dc
LahariReddyMuthyala <bandisairamreddy@icloud.com>
2024-04-27 10:45:11 -0500

    Create FactoryMethod.py

---
+++ Creational Patterns/FactoryMethod.py
@@ -0,0 +1,65 @@
+'''
+
+Courses Website , should return a list of requested modules of a course
+Courses -> Modules
+
+'''
+
+from abc import ABC, abstractmethod
+
+# ---------------------------------------------------------------- factory method fully implemented
+
+class CourseManager(ABC):
+    @abstractmethod
+    def createCourse(self):
+        pass
+
+    def getModules(self):
+        course = self.createCourse()
+        modules = course.getModules()
+
+        return modules
+
+
+class HLDCourseManager(CourseManager):
+    def createCourse(self):
+        return HLD()
+
+
+class LLDCourseManager(CourseManager):
+    def createCourse(self):
+        return LLD()
+
+
+class ICourse(ABC):
+    @abstractmethod
+    def getModules(self):
+        pass
+
+
+class HLD(ICourse):
+    def __init__(self):
+        self.modules = ["a", "b", "c", "d", "e", "f", "g", "h", "i"]
+
+    def getModules(self):
+        return self.modules
+
+
+class LLD(ICourse):
+    def __init__(self):
+        self.modules = ["h", "i"]
+
+    def getModules(self):
+        return self.modules
+
+
+if __name__ == "__main__":
+    # I need a HLD module
+    courseManager = HLDCourseManager()
+    modules = courseManager.getModules()
+    print(modules)
+
+    # I need a LLD module
+    courseManager = LLDCourseManager()
+    modules = courseManager.getModules()
+    print(modules)
